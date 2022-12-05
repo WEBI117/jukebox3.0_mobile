7d200d098296ea7d7956521bfa2c0bfa1254530c
Wahid <wahidejaz@hotmail.com>
2022-12-05 13:47:10 +0500

    Initial app setup with tailwind

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tw`flex h-full w-full justify-center items-center bg-blue-100`}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
