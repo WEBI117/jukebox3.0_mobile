6e9f8b5ec5e4829e7a1bff71ba511b88dc9b96c0
Wahid <wahidejaz@hotmail.com>
2022-12-08 20:02:23 +0500

    alpha version

--- App.tsx
+++ App.tsx
@@ -1,22 +1,129 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
 import tw from 'twrnc'
+import axios from 'axios'
+import { io } from 'socket.io-client'
+const socketUrl = 'http://192.168.0.109:3002'
+const url = 'http://192.168.0.109:3000/'
+const httpsurl = 'https://192.168.0.109:3443/'
 
 export default function App() {
-  return (
-    <View style={tw`flex h-full w-full justify-center items-center bg-blue-100`}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    var [searchText, setSearchText] = useState<string>('Search Song Name')
+    var [songList, setSongList] = useState([])
+    var [socket, setSocket] = useState<any>({})
+    useEffect(() => {
+        console.log('running socket connect logic')
+        var sock = io(socketUrl)
+        sock.on('connect', () => {
+            console.log(`socket connected with id ${socket.id}`)
+        })
+        setSocket(sock)
+    }, [])
+
+    const searchButtonHandler = async () => {
+        try {
+            console.log('sendingRequest')
+            var result = await axios({
+                method: 'get',
+                url: url + 'search',
+                params: {
+                    // TODO: sanitize searchtext before sending to server.
+                    searchstring: searchText
+                }
+            })
+            if (result.status === 200) {
+                setSongList(result.data)
+                console.log(result.data.length)
+            }
+            console.log(result.status)
+        }
+        catch (err) {
+            console.log("err occoured")
+            console.log(err)
+        }
+    }
+
+    const songItemTouchHandler = async (song: any) => {
+        // TODO: Replace HTTP/HTTPS call with socket io.
+        if (socket != null) {
+            socket.emit('addsong', song, (err: any, response: any) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                console.log(response)
+                return
+            })
+        }
+        //try {
+        //    var request = await axios({
+        //        method: 'get',
+        //        url: httpsurl + 'queue/addsong',
+        //        headers: {
+        //            'Content-Type': 'application/json'
+        //        },
+        //        data: {
+        //            "song": JSON.stringify(song)
+        //        }
+        //    })
+        //    if (request.status === 200) {
+        //        console.log(`Song ${song.name} added successfully to queue`)
+        //    }
+        //    console.log(`song add request returned status ${request.status}`)
+        //}
+        //catch (err) {
+        //    console.log('Error occured with song add request')
+        //    console.log(err.message)
+        //}
+    }
+
+    return (
+        <View style={tw`flex flex-col h-full w-full justify-start items-center bg-blue-100`}>
+            <View style={tw`w-full h-1/8`}></View>
+            <View style={tw`flex flex-row justify-center items-center w-full`}>
+                <TextInput
+                    style={tw`w-2/3 border-2 p-4`}
+                    value={searchText}
+                    onChangeText={(value) => {
+                        setSearchText(value)
+                    }}
+                    clearTextOnFocus={true}
+                />
+                <Button
+                    title='Search'
+                    color='black'
+                    onPress={async () => {
+                        await searchButtonHandler()
+                    }}
+                />
+            </View>
+            <View style={tw`w-full h-2`} />
+            <ScrollView style={tw`w-full bg-black`} contentContainerStyle={tw`flex flex-col justify-start items-center p-3`}>
+                {songList.map((song: any) => {
+                    return (
+                        <TouchableOpacity style={tw`flex flex-row w-full h-20 justify-start items-center p-2`} key={song.id}
+                            onPress={() => {
+                                songItemTouchHandler(song)
+                            }}
+                        >
+                            <Text style={tw`text-white`}>
+                                {song.name}
+                            </Text>
+                        </TouchableOpacity>
+                    )
+                })}
+            </ScrollView>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    backgroundColor: '#fff',
+//    alignItems: 'center',
+//    justifyContent: 'center',
+//  },
+//});
