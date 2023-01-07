feb1ecfe1aa18e73c0eeb94199a2ef379c082818
Wahid <wejaz@ssidecisions.com>
2023-01-07 18:01:18 +0500

    search and server connection screen updates for reverse routing.

--- screens/search/searchScreen.tsx
+++ screens/search/searchScreen.tsx
@@ -6,6 +6,9 @@ import CustomScrollableList from '../../components/scrollablelist';
 import screenProps from '../../interfaces/screenProps';
 import httphelper from '../../helpers/httphelper';
 import searchScreenProps from './propsInterface';
+import { screenNames } from '../../constants';
+import navigation from '../../helpers/navigation';
+import queueScreenProps from '../queue/propsInterface';
 
 
 
@@ -14,8 +17,10 @@ import searchScreenProps from './propsInterface';
 const searchScreen = (props: screenProps<searchScreenProps>) => {
     const [searchList, setSearchList] = useState<any[]>(props.propsObj.searchList != undefined ? props.propsObj.searchList : [])
     const [searchText, setSearchText] = useState<string>(props.propsObj.searchText != undefined ? props.propsObj.searchText : '')
+    const [requestLoading, setRequestLoading] = useState<boolean>(false)
 
     const songItemPressHandler = (song: any) => {
+        // TODO: Call Server endpoint to get updated list of songs and ensure added song does not already exist in list.
         var sock = props.propsObj.socket
         if (sock != null) {
             sock.emit('addsong', song, (err: any, response: any) => {
@@ -29,36 +34,81 @@ const searchScreen = (props: screenProps<searchScreenProps>) => {
         }
     }
 
+    // To Queue Screen
+    const BackButtonHandler = () => {
+        var propsToSave: Partial<searchScreenProps> = {
+            serverURL: props.propsObj.serverURL,
+            searchList: searchList,
+            searchText: searchText
+        }
+
+        var propsForNextScreen = navigation.getContext(screenNames.search)
+        navigation.navigate(screenNames.search, propsToSave, screenNames.queue, propsForNextScreen, props.setScreenNameAndProps)
+    }
+
+    // To Queue Screen
+    const navigateForwardHandler = () => {
+        var propsToSave: Partial<searchScreenProps> = {
+            serverURL: props.propsObj.serverURL,
+            searchList: searchList,
+            searchText: ''
+        }
+
+        var propsForNextScreen: Partial<queueScreenProps> = navigation.getContext(screenNames.search)
+        navigation.navigate(screenNames.search, propsToSave, screenNames.queue, propsForNextScreen, props.setScreenNameAndProps)
+    }
+
+    // For Testing
+    useEffect(() => {
+        //console.log(props.propsObj)
+    }, [])
+
+    const loadingContent = () => {
+        if (requestLoading) {
+            return (
+                <Text>Loading...</Text>
+            )
+        }
+        return (
+            <CustomScrollableList
+                data={searchList}
+                renderItem={(song) => {
+                    return (
+                        <TouchableOpacity style={tw`flex flex-row w-full h-10 items-center justify-start`}
+                            onPress={() => { songItemPressHandler(song) }}>
+
+                            <Text>
+                                {song.name}
+                            </Text>
+
+                        </TouchableOpacity>
+                    )
+                }}
+                keyExtractor={(song) => song.id}
+            />
+        )
+    }
+
     return (
         <View style={tw`w-full h-full flex flex-col items-center justify-start`}>
 
             <View style={tw`h-1/12 w-full`}></View>
 
-            <CustomTextInput stateSetter={setSearchText} defaultvalue='Search Song' />
+            <View style={tw`flex flex-row items-center justify-start w-full`}>
+                <Button onPress={() => { BackButtonHandler() }} title='< Back'></Button>
+            </View>
+
+            <CustomTextInput stateSetter={setSearchText} defaultvalue={props.propsObj.searchText != undefined && props.propsObj.searchText != '' ? props.propsObj.searchText : 'Search'} />
             <Button title='Search' onPress={async () => {
+                setRequestLoading(true)
                 setSearchList(await httphelper.getSearchResultFromServer(searchText, props.propsObj.serverURL))
+                setRequestLoading(false)
             }} ></Button>
 
             <View style={tw`w-4 h-1/24`}></View>
 
-
             <View style={tw`h-2/3 w-3/4 bg-blue-200`}>
-                <CustomScrollableList
-                    data={searchList}
-                    renderItem={(song) => {
-                        return (
-                            <TouchableOpacity style={tw`flex flex-row w-full h-10 items-center justify-start`}
-                                onPress={() => { songItemPressHandler(song) }}>
-
-                                <Text>
-                                    {song.name}
-                                </Text>
-
-                            </TouchableOpacity>
-                        )
-                    }}
-                    keyExtractor={(song) => song.name}
-                />
+                {loadingContent()}
             </View>
 
 
--- screens/server_connection/serverConnectionScreen.tsx
+++ screens/server_connection/serverConnectionScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, TextInput, Button, TouchableOpacity } from 'react-native';
 import screenProps from '../../interfaces/screenProps';
-import {screenNames} from '../../constants'
+import { screenNames } from '../../constants'
 import tw from 'twrnc'
 import CustomTextInput from '../../components/textinput'
 import queueScreenProps from '../queue/propsInterface';
 import httphelper from '../../helpers/httphelper';
+import navigation from '../../helpers/navigation';
 import serverConnectionScreenProps from './propsInterface';
 
 const ipaddressreference = 'http://192.168.0.109:3002'
@@ -15,19 +16,53 @@ const serverConnectionScreen = (props: screenProps<serverConnectionScreenProps>)
     const [ipAddress, setIpAddress] = useState<string>(props.propsObj.IpAddress != undefined ? props.propsObj.IpAddress : '')
     const [UserName, setUserName] = useState<string>(props.propsObj.UserName != undefined ? props.propsObj.UserName : '')
 
+    // for testing remove
+    useEffect(() => {
+        //console.log(props.propsObj)
+    },[])
+
+    // To Welcome Screen
+    const BackButtonHandler = () => {
+        var propsToSave: Partial<serverConnectionScreenProps> = {
+            IpAddress: ipAddress,
+            UserName: UserName
+        }
+        var propsToLoad = navigation.getContext(screenNames.welcome)
+        navigation.navigate(screenNames.connectServer, propsToSave, screenNames.welcome, propsToLoad, props.setScreenNameAndProps)
+    }
+
+    // To Queue Screen
+    const navigateForwardHandler = () => {
+        var propsToSave: Partial<serverConnectionScreenProps> = {
+            IpAddress: ipAddress,
+            UserName: UserName
+        }
+        var ipURL = httphelper.convertStringToURL(ipAddress)
+        var propsforqueuescreen: Partial<queueScreenProps> = {
+            // TODO: Remove hardcoded port numbers.
+            socketURL: ipURL + ":3002",
+            serverURL: ipURL + ":3000",
+        }
+        navigation.navigate(screenNames.connectServer, propsToSave, screenNames.queue, propsforqueuescreen, props.setScreenNameAndProps)
+    }
+
     return (
         <View style={tw`flex flex-col justify-start items-center`}>
             {/*seperator*/}
             <View style={tw`h-1/8 w-full`}></View>
+
+            <View style={tw`flex flex-row items-center justify-start w-full`}>
+                <Button onPress={() => { BackButtonHandler() }} title='< Back'></Button>
+            </View>
 
             <Text>Connect to server</Text>
 
             {/*seperator*/}
             <View style={tw`h-1/8 w-full`}></View>
 
-            <CustomTextInput stateSetter={setIpAddress} defaultvalue='IP Address' />
+            <CustomTextInput stateSetter={setIpAddress} defaultvalue={props.propsObj.IpAddress != undefined && props.propsObj.IpAddress != '' ? props.propsObj.IpAddress : 'Ip Address'} />
 
-            <CustomTextInput stateSetter={setUserName} defaultvalue='User Name' />
+            <CustomTextInput stateSetter={setUserName} defaultvalue={props.propsObj.UserName != undefined && props.propsObj.UserName != '' ? props.propsObj.UserName : 'Username'} />
 
             {/*seperator*/}
             <View style={tw`h-1/8 w-full`}></View>
@@ -35,23 +70,11 @@ const serverConnectionScreen = (props: screenProps<serverConnectionScreenProps>)
             {/* Button to navigate to another screen....may extract into a custom component*/}
             <View style={tw`w-2/3 h-1/12 p-3`}>
                 <TouchableOpacity style={tw`bg-blue-200 flex flex-row justify-center items-center w-full h-full`}
-                    onPress={() => {
-                        
-                        var ipURL = httphelper.convertStringToURL(ipAddress)
-                        var propsforqueuescreen: Partial<queueScreenProps> = {
-                            // TODO: Remove hardcoded port numbers.
-                            socketURL: ipURL + ":3002",
-                            serverURL: ipURL + ":3000",
+                    onPress={
+                        () => {
+                            navigateForwardHandler()
                         }
-
-                        props.setScreenNameAndProps(
-                            {
-                                screenName: screenNames.queue,
-                                // TODO: remove hard coded string for testing
-                                props: propsforqueuescreen
-                            }
-                        )
-                    }}>
+                    }>
                     <Text>Connect</Text>
                 </TouchableOpacity>
             </View>
