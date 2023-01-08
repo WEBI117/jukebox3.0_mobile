7dfc8f04ebe09544573a4699e643c6cfa44996bd
Wahid <wejaz@ssidecisions.com>
2023-01-08 12:46:56 +0500

    custom twnrc object with color scheme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'cpink': {
-        100: '#FF008C',
-        200: '#BF026B',
-        300: '#80054A',
-        400: '#400729',
-      },
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'white': '#ffffff',
+                'cpink': {
+                    100: '#FF008C',
+                    200: '#BF026B',
+                    300: '#80054A',
+                    400: '#400729',
+                },
+            },
+        },
     },
-  },
 }
